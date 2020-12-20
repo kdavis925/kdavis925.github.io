73c4f18a5b0571c9b89fcaa1a41c6f713fdbeceb
Ethan Marsh <emarsh55@gmail.com>
2020-12-19 16:22:22 -0800

    Fix build errors

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 import Page from './Page';
 
-export default () => (
-  <BrowserRouter>
-    <Switch>
-      <Route render={({ location }) => <Page pathname={location.pathname} />} />
-    </Switch>
-  </BrowserRouter>
-);
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route render={({ location }) => <Page pathname={location.pathname} />} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
